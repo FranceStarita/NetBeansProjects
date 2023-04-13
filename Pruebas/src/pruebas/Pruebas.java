77ac2ef51b068306fcb2e998033a7b3fa62bf580
Juan Alexander Agreda <juan3aad@gmail.com>
2023-04-12 23:25:47 -0500

    Todos tienen los cambios

--- Pruebas/src/pruebas/Pruebas.java
+++ Pruebas/src/pruebas/Pruebas.java
@@ -18,7 +18,7 @@ public class Pruebas {
         // TODO code application logic here
         System.out.println("David ");
         System.out.println("Diego ");
-        System.out.println("Todosd");
+        System.out.println("Todos listos");
         
         //Modificado por Elimes R
         Scanner leer = new Scanner(System.in);
